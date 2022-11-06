5fd6e439ccb169f63b02b4549221b3eb396e740e
fha96 <fahadalrimawe96@hotmail.com>
2022-11-06 22:38:41 +0200

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState , useEffect} from 'react';
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3001');
+
 
 function App() {
+  const [message, setMessage] = useState();
+  const [receinvedMessage, setReceivedMessage] = useState();
+
+  const handleSend = () => {
+    socket.emit('message',{message: message})
+  };
+
+  useEffect(() => {
+    socket.on('user_disconnected', (payload) => {
+      alert(payload.disconn);
+    });
+    socket.on('new_user_connected', payload => {
+      alert(`New user connected with username: ${payload.user}`)
+    });
+    socket.on('received', (payload) => {
+      setReceivedMessage(preValue => preValue = payload.message)
+    });
+  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form onSubmit={handleSend}>
+      <input placeholder='Message...  ' name='message' onChange={(e) =>setMessage(e.target.value)}/>
+      <input type='button' value='Send Message' onClick={handleSend}/>
+      </form>
+      {
+        receinvedMessage && 
+        <h2> { receinvedMessage } </h2>
+      }
     </div>
   );
 }
